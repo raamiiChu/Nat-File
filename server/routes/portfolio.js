e8d06f60db7305aa8cd301f328e232e2cc910074
raamiiChu <ericchu0617@gmail.com>
2023-11-28 21:55:10 +0800

    Feat: portfolio router

---
+++ server/routes/portfolio.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { User, Portfolio } from "../models/index.js";
+const route = Router();
+
+route.use((req, res, next) => {
+    next();
+});
+
+route.post("/save", async (req, res) => {
+    const { email, images, layouts } = req.body;
+
+    const foundUser = await User.findOne({ where: { email } });
+
+    const newPortfolio = await Portfolio.create({ images, layouts });
+    await newPortfolio.setUser(foundUser);
+
+    res.send("ok");
+});
+
+route.get("/load", async (req, res) => {
+    try {
+        let rawData = await User.findOne({
+            where: { id: 1 },
+            attributes: ["name", "email"],
+            include: [{ model: Portfolio, attributes: ["images", "layouts"] }],
+        });
+
+        return res.status(200).send(rawData);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send("something wrong");
+    }
+});
+
+export default route;
