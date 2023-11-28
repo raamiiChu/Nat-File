8fc01c7dc4dfc6685fed75711d6d3b44b8a2e543
raamiiChu <ericchu0617@gmail.com>
2023-11-28 21:55:30 +0800

    Feat: database connection

---
+++ server/db.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+    }
+);
+
+try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
+
+export default sequelize;
