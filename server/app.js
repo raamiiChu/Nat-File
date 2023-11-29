4dc86d1effb9da4ec2b9eb5a91fbb07be0368265
raamiiChu <ericchu0617@gmail.com>
2023-11-29 14:30:31 +0800

    Build: cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 import "dotenv/config";
 import express from "express";
+
+import cors from "cors";
+
 import sequelize from "./db.js";
 
 import { userRoute, portfolioRoute } from "./routes/index.js";
 
 const app = express();
 const port = 3001;
+const corsOptions = {
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 sequelize.sync();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/user", userRoute);
 app.use("/portfolio", portfolioRoute);
