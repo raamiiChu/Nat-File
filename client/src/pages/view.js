4515324bfd2d09d9003d09b8cbb2350d5cefb780
raamiiChu <ericchu0617@gmail.com>
2023-12-02 17:13:10 +0800

    Fix: scene bind on specific div

--- client/src/pages/view.js
+++ client/src/pages/view.js
@@ -1,49 +1,63 @@
 import React, { useState, useEffect } from "react";
 
 import * as THREE from "three";
-import vertexShader from "../shaders/vertex.glsl";
+// import vertexShader from "../shaders/vertex.glsl";
 
 import earth from "../images/earth.jpg";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+const vertexShader = `
+    void main(){
+        gl_Position = vec4(1, 0, 0, 1);
+    }
+`;
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-renderer.setPixelRatio(window.devicePixelRatio);
+console.log(vertexShader);
 
-document.body.appendChild(renderer.domElement);
-
-// create a sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
-    new THREE.ShaderMaterial({
-        vertexShader: vertexShader,
-        // fragmentShader: "",
-    })
-);
-
-scene.add(sphere);
-
-// reset position of camera
-// make sure the value should greater than 1st param of THREE.SphereGeometry()
-camera.position.z = 13;
-
-// custom shader
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate();
 const View = () => {
-    useEffect(() => {});
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            70,
+            width / height,
+            0.01,
+            10
+        );
 
-    return <div></div>;
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(width, height);
+        renderer.setPixelRatio(window.devicePixelRatio);
+
+        document.getElementById("earth").appendChild(renderer.domElement);
+
+        // create a sphere
+        const sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(5, 50, 50),
+            new THREE.ShaderMaterial({
+                vertexShader,
+                // fragmentShader: "",
+            })
+        );
+
+        scene.add(sphere);
+
+        // reset position of camera
+        // make sure the value should greater than 1st param of THREE.SphereGeometry()
+        camera.position.z = 13;
+
+        // custom shader
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    });
+
+    return <div id="earth"></div>;
 };
 
 export default View;
