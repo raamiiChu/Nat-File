8d53127698f71a8ca1b786158914e1ab178a86d7
raamiiChu <ericchu0617@gmail.com>
2023-12-02 23:06:02 +0800

    Style: shaders

--- client/src/shaders/vertex.glsl
+++ client/src/shaders/vertex.glsl
@@ -1,3 +1,8 @@
-void main(){
-    gl_Position = vec4(1, 0, 0, 1);
+varying vec2 vertexUV;
+varying vec3 vertexNormal;
+
+void main() {
+    vertexUV = uv;
+    vertexNormal = normalize(normalMatrix * normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
