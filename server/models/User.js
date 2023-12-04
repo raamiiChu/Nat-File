e87c5bb8eff4af69444c6b2607fd10988d288cde
raamiiChu <ericchu0617@gmail.com>
2023-12-04 15:27:32 +0800

    Style: remove user name

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,6 @@ import sequelize from "../db.js";
 import { DataTypes } from "sequelize";
 
 const User = sequelize.define("User", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
