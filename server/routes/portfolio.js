51467bf0a9f3cc9111aa653e96974852654e8ce1
raamiiChu <ericchu0617@gmail.com>
2023-12-05 15:08:55 +0800

    Feat: get all portfolio by user id

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -35,15 +35,33 @@ route.post("/load", async (req, res) => {
 
             return res.status(200).send(newPortfolio);
         } else {
-            const portfolio = await Portfolio.findOne({
-                where: { id },
+            const rawData = await User.findOne({
+                where: { email },
+                attributes: ["email"],
+                include: [{ model: Portfolio, where: { id } }],
             });
 
-            return res.status(200).send(portfolio);
+            return res.status(200).send(rawData.Portfolios[0]);
         }
     } catch (error) {
         console.log(error);
         return res.status(400).send("something wrong");
+    }
+});
+
+route.get("/loadAll/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const rawData = await User.findOne({
+            where: { id },
+            attributes: ["email"],
+            include: [{ model: Portfolio }],
+        });
+
+        return res.status(200).send(rawData);
+    } catch (error) {
+        return res.status(500).send(error);
     }
 });
 
