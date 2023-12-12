c51e3f4b3aa4b02d56b49889d1589b24a68157c8
raamiiChu <ericchu0617@gmail.com>
2023-12-12 18:46:54 +0800

    Feat: button to get public url

--- client/src/components/profile/portfolioItem.js
+++ client/src/components/profile/portfolioItem.js
@@ -5,10 +5,19 @@ import { format } from "date-fns";
 
 import { MdOutlineModeEdit } from "react-icons/md";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { FiLink } from "react-icons/fi";
 import imageNotFound from "../../images/image-not-found.jpg";
 
+import Swal from "sweetalert2";
+
 const PortfolioItem = ({ userId, portfolio, toEditPage, deletePortfolio }) => {
-    const { id: portfolioId, images, createdAt, updatedAt } = portfolio;
+    const {
+        id: portfolioId,
+        publicId,
+        images,
+        createdAt,
+        updatedAt,
+    } = portfolio;
 
     let title, s3Url;
     if (images[0]) {
@@ -17,10 +26,18 @@ const PortfolioItem = ({ userId, portfolio, toEditPage, deletePortfolio }) => {
         title = images[0].title;
     }
 
+    const getPublicUrl = (e, publicId) => {
+        e.preventDefault();
+        Swal.fire({
+            title: "Here's Your Public Link",
+            text: `${window.location.origin}/share/${publicId}`,
+        });
+    };
+
     return (
         <div
             key={`user-${userId}_portfolio-${portfolioId}`}
-            className="group relative grid grid-cols-12 gap-y-3 px-3 py-5 text-gray bg-white border-2 border-solid border-white border-opacity-50 rounded-3xl shadow-lg"
+            className="group relative grid grid-cols-12 gap-y-3 px-3 py-5 text-gray bg-white border-2 border-solid border-white border-opacity-50 rounded-3xl shadow-blur shadow-slate-500"
         >
             <section className="col-start-2 col-span-10">
                 <img
@@ -29,6 +46,38 @@ const PortfolioItem = ({ userId, portfolio, toEditPage, deletePortfolio }) => {
                     className="block object-contain mx-auto max-h-40 rounded-lg"
                 />
             </section>
+
+            <nav className="col-start-2 col-span-10 flex justify-center gap-x-5">
+                <Link
+                    className="w-10 h-10 flex justify-center items-center border border-solid border-black rounded-full bg-white text-black hover:bg-black hover:text-primary hover:scale-110 transition-all duration-300"
+                    title="Edit"
+                    onClick={(e) => {
+                        toEditPage(e, portfolioId);
+                    }}
+                >
+                    <MdOutlineModeEdit className="text-xl" />
+                </Link>
+
+                <Link
+                    className="w-10 h-10 flex justify-center items-center border border-solid border-black rounded-full bg-white text-black hover:bg-black hover:text-primary hover:scale-110 transition-all duration-300"
+                    title="Share"
+                    onClick={(e) => {
+                        getPublicUrl(e, publicId);
+                    }}
+                >
+                    <FiLink className="text-xl" />
+                </Link>
+
+                <Link
+                    className="w-10 h-10 flex justify-center items-center border border-solid border-black rounded-full bg-white text-black hover:bg-red-500 hover:text-primary hover:scale-110 transition-all duration-300"
+                    title="Delete"
+                    onClick={(e) => {
+                        deletePortfolio(e, portfolioId, images);
+                    }}
+                >
+                    <FaRegTrashAlt className="text-xl" />
+                </Link>
+            </nav>
 
             <article className="col-span-full">
                 <h3 className="text-lg">Images: {images.length}</h3>
@@ -45,26 +94,6 @@ const PortfolioItem = ({ userId, portfolio, toEditPage, deletePortfolio }) => {
                     </span>
                 </p>
             </article>
-
-            <Link
-                className="opacity-0 group-hover:opacity-100 absolute -top-3 -left-3 w-12 h-12 flex justify-center items-center border-e border-b border-solid border-black rounded-full bg-primary text-black hover:shadow-surrounding hover:shadow-slate-500 transition-all duration-300"
-                title="Edit"
-                onClick={(e) => {
-                    toEditPage(e, portfolioId);
-                }}
-            >
-                <MdOutlineModeEdit className="w-6 h-6" />
-            </Link>
-
-            <Link
-                className="opacity-0 group-hover:opacity-100 absolute -top-3 -right-3 w-12 h-12 flex justify-center items-center border-s border-b border-solid border-black rounded-full bg-white text-black hover:shadow-surrounding hover:shadow-red-500 transition-all duration-300"
-                title="Delete"
-                onClick={(e) => {
-                    deletePortfolio(e, portfolioId, images);
-                }}
-            >
-                <FaRegTrashAlt className="w-6 h-6" />
-            </Link>
         </div>
     );
 };
